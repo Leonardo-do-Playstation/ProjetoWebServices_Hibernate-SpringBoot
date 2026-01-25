44f7e94035cfec9fe6f54b85917920b400a54a1f
Leonardo Antonio de Oliveira Boff <leooliveiraboff@gmail.com>
2026-01-25 19:28:22 -0300

    Service layer and component registration

--- src/main/java/br/com/leonardoboff/UpperSellerProject/repositories/UserRepository.java
+++ src/main/java/br/com/leonardoboff/UpperSellerProject/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package br.com.leonardoboff.UpperSellerProject.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.leonardoboff.UpperSellerProject.entities.User;
 
+@Repository //Opcional
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
---
+++ src/main/java/br/com/leonardoboff/UpperSellerProject/services/UserService.java
@@ -0,0 +1,25 @@
+package br.com.leonardoboff.UpperSellerProject.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.leonardoboff.UpperSellerProject.entities.User;
+import br.com.leonardoboff.UpperSellerProject.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.get();
+	}
+}
