06a7b33e375a51305dedbd2d468893d980366333
Leonardo Antonio de Oliveira Boff <leooliveiraboff@gmail.com>
2026-01-25 14:43:19 -0300

    Project created

---
+++ src/main/java/br/com/leonardoboff/UpperSellerProject/UpperSellerProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.leonardoboff.UpperSellerProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpperSellerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpperSellerProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/leonardoboff/UpperSellerProject/UpperSellerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leonardoboff.UpperSellerProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpperSellerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
