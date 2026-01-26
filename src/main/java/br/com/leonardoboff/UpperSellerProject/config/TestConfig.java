fea33dc350799dab4a4b4c8076aae1bb6929e6ed
Leonardo Antonio de Oliveira Boff <leooliveiraboff@gmail.com>
2026-01-26 18:51:48 -0300

    Create Payment class and association one-to-one with Order class

--- src/main/java/br/com/leonardoboff/UpperSellerProject/config/TestConfig.java
+++ src/main/java/br/com/leonardoboff/UpperSellerProject/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.com.leonardoboff.UpperSellerProject.entities.Category;
 import br.com.leonardoboff.UpperSellerProject.entities.Order;
 import br.com.leonardoboff.UpperSellerProject.entities.OrderItem;
+import br.com.leonardoboff.UpperSellerProject.entities.Payment;
 import br.com.leonardoboff.UpperSellerProject.entities.Product;
 import br.com.leonardoboff.UpperSellerProject.entities.User;
 import br.com.leonardoboff.UpperSellerProject.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
