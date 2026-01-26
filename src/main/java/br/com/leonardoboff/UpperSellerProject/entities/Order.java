c98109b11daafee0ca7927245f06a2f865d6494a
Leonardo Antonio de Oliveira Boff <leooliveiraboff@gmail.com>
2026-01-26 19:08:02 -0300

    Implement subTotal and total methods

--- src/main/java/br/com/leonardoboff/UpperSellerProject/entities/Order.java
+++ src/main/java/br/com/leonardoboff/UpperSellerProject/entities/Order.java
@@ -99,6 +99,14 @@ public class Order implements Serializable {
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for(OrderItem obj: items) {
+			sum += obj.getSubTotal();
+		}
+		return sum;
+	}
 
 	@Override
 	public int hashCode() {
